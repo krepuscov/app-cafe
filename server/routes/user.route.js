704e570624c6a8766534231b7d7364ea9d199b5b
Vuk Krepusco <51110114+krepuscov@users.noreply.github.com>
2019-05-28 17:53:06 -0500

    Authorization with token (#4)
    # Add
    
    * Sent token in login
    * check token y ADMIN_ROLE in post and delete methods

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,15 +2,16 @@ const express = require('express')
 const app = express()
 const User = require('../models/user.model')
 let user_controller = require('../controllers/user.controller')
+const { checkToken, checkAdminRole } = require('../middlewares/authorization')
 
 app.get('/', (req, res) => {})
 
 app.get('/user', user_controller.getUser)
 
-app.post('/user', user_controller.postUser)
+app.post('/user', checkToken, checkAdminRole, user_controller.postUser)
 
 app.put('/user/:id', user_controller.putUser)
 
-app.delete('/user/:id', user_controller.deleteUser)
+app.delete('/user/:id', checkToken, checkAdminRole, user_controller.deleteUser)
 
 module.exports = app
